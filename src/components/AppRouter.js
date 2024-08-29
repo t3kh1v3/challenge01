638123075e54dbc9f66cb7e429932ed4eb497dc3
David Moura <mourazdavid@gmail.com>
2024-08-29 12:51:33 -0300

    Revert "listPage"
    This reverts commit f56c123f512256ae3509fd85eb923e69bb56689d.

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,52 +1,51 @@
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import {Home} from '../pages/home/Home';
-import {List} from "../pages/home/List";
-import {Layout} from './Layout';
+import { Home } from '../pages/home/Home';
+import { Layout } from './Layout';
 import React from 'react';
 
 export const AppRouter = () => (
-    <Router>
-        <Routes>
-            <Route path='/'
-                   element={(
-                       <Layout>
-                           <Home/>
-                       </Layout>
-                   )}
-            />
+  <Router>
+    <Routes>
+      <Route path='/'
+        element={(
+          <Layout>
+            <Home />
+          </Layout>
+        )}
+      />
 
-            <Route path='group'
-                   element={(
-                       <Layout>
-                           <div>GROUP & TECH</div>
-                       </Layout>
-                   )}
-            />
+      <Route path='group'
+        element={(
+          <Layout>
+            <div>GROUP & TECH</div>
+          </Layout>
+        )}
+      />
 
-            <Route path='contact'
-                   element={(
-                       <Layout>
-                           <div>CONTATO</div>
-                       </Layout>
-                   )}
-            />
+      <Route path='contact'
+        element={(
+          <Layout>
+            <div>CONTATO</div>
+          </Layout>
+        )}
+      />
 
-            <Route path='list'
-                   element={(
-                       <Layout>
-                           <List/>
-                       </Layout>
-                   )}
-            />
+      <Route path='list'
+        element={(
+          <Layout>
+            <div>LIST</div>
+          </Layout>
+        )}
+      />
 
-            <Route path='project'
-                   element={(
-                       <Layout>
-                           <div>PROJECT</div>
-                       </Layout>
-                   )}
-            />
-        </Routes>
-    </Router>
+      <Route path='project'
+        element={(
+          <Layout>
+            <div>PROJECT</div>
+          </Layout>
+        )}
+      />
+    </Routes>
+  </Router>
 );
