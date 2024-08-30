f1ed4e0fedb6b36fe4ead76c9767b911b84f6e5e
kaiquebonini <ks.bonini@gmail.com>
2024-08-29 22:53:59 -0300

    fix list

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,51 +1,51 @@
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
 import {Home} from '../pages/home/Home';
-import {List} from "../pages/home/List";
 import {Layout} from './Layout';
+import {List} from "../pages/home/list/List";
 import React from 'react';
 
 export const AppRouter = () => (
     <Router>
         <Routes>
             <Route path='/'
-                   element={(
-                       <Layout>
-                           <Home/>
-                       </Layout>
-                   )}
+                element={(
+                    <Layout>
+                        <Home/>
+                    </Layout>
+                )}
             />
 
             <Route path='group'
-                   element={(
-                       <Layout>
-                           <div>GROUP & TECH</div>
-                       </Layout>
-                   )}
+                element={(
+                    <Layout>
+                        <div>GROUP & TECH</div>
+                    </Layout>
+                )}
             />
 
             <Route path='contact'
-                   element={(
-                       <Layout>
-                           <div>CONTATO</div>
-                       </Layout>
-                   )}
+                element={(
+                    <Layout>
+                        <div>CONTATO</div>
+                    </Layout>
+                )}
             />
 
             <Route path='list'
-                   element={(
-                       <Layout>
-                           <List/>
-                       </Layout>
-                   )}
+                element={(
+                    <Layout>
+                        <List/>
+                    </Layout>
+                )}
             />
 
             <Route path='project'
-                   element={(
-                       <Layout>
-                           <div>PROJECT</div>
-                       </Layout>
-                   )}
+                element={(
+                    <Layout>
+                        <div>PROJECT</div>
+                    </Layout>
+                )}
             />
         </Routes>
     </Router>
